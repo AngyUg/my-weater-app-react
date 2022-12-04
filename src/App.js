f618d9a53f15ae56d10e7e1c0e377b492461ae24
AngyUg <40401933+AngyUg@users.noreply.github.com>
2022-12-04 19:37:21 +0200

    add  link to my GitHub repository

--- src/App.js
+++ src/App.js
@@ -7,18 +7,15 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <SearchEngine />
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/AngyUg/my-weater-app-react"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React harder
+          go to GitHub repo...
         </a>
-        <SearchEngine />
       </header>
     </div>
   );
