92adab1671b3cb4de51579c69a71c41d3c82f4b4
AngyUg <40401933+AngyUg@users.noreply.github.com>
2022-12-04 22:21:04 +0200

    import bootstrap

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from "./logo.svg";
 import "./App.css";
 import SearchEngine from "./SearchEngine";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <SearchEngine />
-        <a
-          className="App-link"
-          href="https://github.com/AngyUg/my-weater-app-react"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          go to GitHub repo...
-        </a>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <SearchEngine />
+          <a
+            className="App-link"
+            href="https://github.com/AngyUg/my-weater-app-react"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            go to GitHub repo...
+          </a>
+        </header>
+      </div>
     </div>
   );
 }
