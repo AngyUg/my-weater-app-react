7195e71fa71ce310988350f5db0e076bb1d20685
AngyUg <40401933+AngyUg@users.noreply.github.com>
2022-12-11 20:51:41 +0200

    add  rows for weather descriptions and css

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,15 +1,25 @@
 import React, { useState } from "react";
 import axios from "axios";
+import "./SearchEngine.css";
 
 export default function SearchEngine() {
   let [text, setText] = useState({});
   let [city, setCity] = useState("");
   let [loaded, setLoaded] = useState(false);
 
+  let dayTimeNow = new Date().toLocaleDateString("en-us", {
+    weekday: "long",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false,
+  });
+
   function showDataWeather(response) {
     setLoaded(true);
     console.log(response);
     setText({
+      city: response.data.name,
+      country: response.data.sys.country,
       temperature: response.data.main.temp,
       description: response.data.weather[0].description,
       humidity: response.data.main.humidity,
@@ -33,22 +43,55 @@ export default function SearchEngine() {
 
   let form = (
     <form onSubmit={handleSubmit}>
-      <input type="search" placeholder="City..." onChange={updateCity} />
-      <button type="submit">Search</button>
+      <div class="row">
+        <div class="col-9">
+          <input
+            className="form-control"
+            type="search"
+            placeholder="City..."
+            onChange={updateCity}
+          />
+        </div>
+        <div class="col-3">
+          {" "}
+          <button type="submit" className="btn btn-primary">
+            Search
+          </button>
+        </div>
+      </div>
     </form>
   );
 
   if (loaded) {
     return (
-      <div>
+      <div className="SearchEngine">
         {form}
-        <div>
-          <div>Temperature {Math.round(text.temperature)}°C</div>{" "}
-          <div>Description: "{text.description}"</div>
-          <div>Humidity: {Math.round(text.humidity)}%</div>
-          <div>Wind: {text.wind}km/h</div>
-          <div>
+        <div className="row">
+          <div className="col-12">
+            {" "}
+            <h1>
+              {text.city}, {text.country}
+            </h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-6">
+            <ul>
+              <div>Today is {dayTimeNow}</div>
+              <div>{text.description}</div>
+            </ul>
+          </div>
+          <div className="col-6">
+            <div>Humidity: {Math.round(text.humidity)}%</div>
+            <div>Wind: {text.wind}km/h</div>
+          </div>
+        </div>
+        <div className="col-6">
+          <div className="clearfix weather-temperature">
             <img src={text.icon} alt={text.description} />
+            <span className="temperature">
+              {Math.round(text.temperature)}°C
+            </span>
           </div>
         </div>
       </div>
