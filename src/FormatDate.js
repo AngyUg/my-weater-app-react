0fdd3be4c9cbe918a8ea493fe6d198d59d4095ac
AngyUg <40401933+AngyUg@users.noreply.github.com>
2022-12-16 22:46:24 +0200

    add component with current day and time

---
+++ src/FormatDate.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+export default function FormatDate() {
+  let dayTimeNow = new Date().toLocaleDateString("en-us", {
+    weekday: "long",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false,
+  });
+  return dayTimeNow;
+}
