7ad0854dac80e7aeb17a5fcf2bcb3a4b07572cb4
AngyUg <40401933+AngyUg@users.noreply.github.com>
2022-12-25 00:59:30 +0200

    fix footer

--- src/App.js
+++ src/App.js
@@ -8,24 +8,6 @@ function App() {
     <div className="App">
       <div className="container">
         <SearchEngine defCity="Barcelona" />
-        <footer>
-          Coded by{" "}
-          <a
-            href="https://www.linkedin.com/in/angelina-yuga-226929187/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Angelina Yuga
-          </a>{" "}
-          open-sourced on{" "}
-          <a
-            href="https://github.com/AngyUg/my-weater-app-react"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            GitHub
-          </a>
-        </footer>
       </div>
     </div>
   );
